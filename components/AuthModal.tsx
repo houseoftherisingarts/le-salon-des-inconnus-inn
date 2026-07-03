a1758366563ae63d10b4183510a6aef7597b2d05
alextstlaurent <alex@lesalondesinconnus.com>
2026-07-03 12:43:48 -0400

    autosave: 2026-07-03 12:43:48

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -7,6 +7,7 @@ import {
   signInWithRedirect,
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
+  sendEmailVerification,
   updateProfile,
   RecaptchaVerifier,
   signInWithPhoneNumber,
