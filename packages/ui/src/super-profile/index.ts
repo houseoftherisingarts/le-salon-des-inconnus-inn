1e336f44b8adcc0667b24db1c04f2cb9dfcd22bb
alextstlaurent <alex@lesalondesinconnus.com>
2026-09-12 12:34:15 -0400

    autosave: 2026-09-12 12:34:15

--- packages/ui/src/super-profile/index.ts
+++ packages/ui/src/super-profile/index.ts
@@ -7,8 +7,22 @@ export type {
     SuperProfileHeroPhoto,
     SuperProfileWork,
     UsernameClaim,
+    ArtistType,
+    SectionId,
+    SectionsConfig,
+    OeuvreProfilPro,
+    LienEcoute,
+    PlateformeEcoute,
+    DateSpectacle,
+    CitationPresse,
+    ExpositionProfilPro,
+    LivreProfilPro,
+    AtelierProfilPro,
 } from './types';
-export { MAX_WORKS, heroStoragePath, workStoragePath } from './types';
+export {
+    MAX_WORKS, MAX_OEUVRES, MAX_DATES, MAX_LIENS_ECOUTE, MAX_EXPOSITIONS, MAX_LIVRES,
+    heroStoragePath, workStoragePath, oeuvreStoragePath, couvertureLivreStoragePath,
+} from './types';
 
 export {
     validateUsername,
@@ -20,6 +34,29 @@ export {
     RESERVED_SLUGS,
 } from './usernames';
 
+export {
+    LIBELLES_ARTISTE, ORDRE_TYPES, deduireType, familleGabarit, sectionsParDefaut, sectionAllumee,
+} from './artistes';
+export type { LibelleArtiste, FamilleGabarit } from './artistes';
+
+export {
+    normaliserHostname, validerHostname, domaineDe, reserverDomaine, retirerDomaine, resoudreHostname,
+} from './domaines';
+export type { DomaineClaim, ValidationHostname } from './domaines';
+
+export {
+    AGENDA_PAR_DEFAUT, cleJour, plagesDuJour, creneauxLibres, joursDisponibles,
+    nomSalle, salleUrl, rencontreOuverte, formatDate, formatHeure, icsRendezVous, telechargerIcs,
+    lireAgendaConfig, useAgendaConfig, useOccupationsPro, useMesRendezVousPro, useRendezVousProArtiste,
+    demanderRendezVous, annulerRendezVousPro, majRendezVousProParArtiste, enregistrerAgendaConfig,
+} from './rendezvous';
+export type { PlageHoraire, AgendaConfig, StatutRendezVousPro, RendezVousPro, OccupationPro, Creneau } from './rendezvous';
+
+export {
+    ouvrirCheckoutProfilPro, ouvrirPortailProfilPro, useAbonnementPro, useMemberFlags, profilProDebloque,
+} from './abonnement';
+export type { AbonnementPro, StatutAbonnementPro } from './abonnement';
+
 export { SuperProfileEditor } from './SuperProfileEditor';
 export type { SuperProfileEditorProps } from './SuperProfileEditor';
 
@@ -28,6 +65,20 @@ export { RevealWaveImage } from './RevealWaveImage';
 export { InteractivePhotoStack } from './InteractivePhotoStack';
 export type { PhotoStackItem, InteractivePhotoStackProps } from './InteractivePhotoStack';
 
-export { PhotoTemplate } from './templates/PhotoTemplate';
-export { VisualArtTemplate } from './templates/VisualArtTemplate';
-export { EditorialTemplate } from './templates/EditorialTemplate';
+export { Rencontre } from './Rencontre';
+export { PriseRendezVous } from './PriseRendezVous';
+export { AgendaAdmin } from './AgendaAdmin';
+export { RendezVousAdmin } from './RendezVousAdmin';
+export { DomaineAdmin } from './DomaineAdmin';
+export { UpsellCard } from './UpsellCard';
+export { ProfilProAdmin } from './ProfilProAdmin';
+export type { ProfilProAdminProps } from './ProfilProAdmin';
+export { ProfilProAdminSite } from './ProfilProAdminSite';
+export { ProfilProAdminContenu } from './ProfilProAdminContenu';
+export { ProfilProAdminAbonnement } from './ProfilProAdminAbonnement';
+export { ProfilProPage } from './ProfilProPage';
+export type { ProfilProPageProps } from './ProfilProPage';
+
+export { gabaritPour, PeintreTemplate, MusicienTemplate, PhotoTemplate, VisualArtTemplate, EditorialTemplate } from './templates';
+export * from './templates/sections';
+export type { TemplateProps } from './templates';
