aee240f1adbeecf8c9b07c6ea746412718a3b6ae
alextstlaurent <alex@lesalondesinconnus.com>
2026-09-12 12:19:19 -0400

    autosave: 2026-09-12 12:19:19

---
+++ packages/ui/src/reseau/index.ts
@@ -0,0 +1,22 @@
+// Réseau social d'artistes du Creator Studio — barrel du paquet.
+export * from './auth';
+export * from './membres';
+export * from './images';
+export * from './mur';
+export * from './moderation';
+export * from './notifications';
+export * from './badges';
+export * from './amities';
+export * from './dms';
+
+export { Avatar } from './Avatar';
+export { VoteBar } from './VoteBar';
+export { MurSocial } from './MurSocial';
+export { BilletCarte } from './BilletCarte';
+export { Commentaires } from './Commentaires';
+export { Composeur } from './Composeur';
+export { Cloche } from './Cloche';
+export { VitrineBadges, SelecteurBadges, PanneauAdminBadges } from './Badges';
+export { ProfilSocial } from './ProfilSocial';
+export { MonProfilForm } from './MonProfilForm';
+export { PageMembre } from './PageMembre';
