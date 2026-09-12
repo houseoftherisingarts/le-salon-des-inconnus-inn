b257ef9abfca498d2694e03f6ca60ec82b72cdab
alextstlaurent <alex@lesalondesinconnus.com>
2026-09-12 12:13:01 -0400

    autosave: 2026-09-12 12:13:01

---
+++ packages/ui/src/super-profile/templates/sections/index.ts
@@ -0,0 +1,17 @@
+// Barrel des sections du Profil Pro : un seul import pour les quatre gabarits.
+
+export { HeroSection } from './Hero';
+export { OeuvresSection } from './Oeuvres';
+export { EcouteSection } from './Ecoute';
+export { DatesSection } from './Dates';
+export { PresseSection } from './Presse';
+export { ExpositionsSection } from './Expositions';
+export { LivresSection } from './Livres';
+export { BioSection } from './Bio';
+export { AtelierSection } from './Atelier';
+export { PriseRendezVousSection } from './PriseRendezVous';
+export { ContactSection } from './Contact';
+export { LiensSection } from './Liens';
+export { PiedDePageSection } from './PiedDePage';
+export { sectionVisible, SectionShell, SectionTitle, CARD_GLASS, formatPrixCents, formatDateCourte } from './common';
+export type { SectionProps } from './common';
